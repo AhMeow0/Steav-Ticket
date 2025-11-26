1e1f3ad463c1065e44e4c7f8385815fdc8a0690b
Lythong <thongking1280@gmail.com>
2025-11-26 23:15:44 +0700

    Delete Backend folder

--- Backend/dist/models/Route.js
+++
@@ -1,9 +0,0 @@
-import { Schema, model } from "mongoose";
-const routeSchema = new Schema({
-    from: { type: String, required: true },
-    to: { type: String, required: true },
-    price: { type: Number, required: true },
-    duration: { type: String, required: true }, // "6h 30m"
-});
-export default model("Route", routeSchema);
-//# sourceMappingURL=Route.js.map
