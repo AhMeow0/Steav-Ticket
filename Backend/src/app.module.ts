3f89c0b641ba201392dae400d4db74c53fb21a56
Thongking <thongking1280@gmail.com>
2025-12-18 17:45:08 +0700

    Backend run test

--- Backend/src/app.module.ts
+++ Backend/src/app.module.ts
@@ -4,12 +4,16 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TicketsModule } from './tickets/tickets.module';
 import { RoutesModule } from './routes/routes.module';
+import { UsersModule } from './users/users.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [
     MongooseModule.forRoot('mongodb://localhost:27017/bus-ticket'),
     TicketsModule,
     RoutesModule,
+    UsersModule,
+    AuthModule,
   ],
   controllers: [AppController],
   providers: [AppService],
